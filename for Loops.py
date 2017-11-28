0091432bb67f49bdcfb0221d671b5a940304ef45
oneyedananas <mart.vokal@gmail.com>
2017-11-28 23:59:52 +0100

    for loops excercise 1

--- for Loops.py
+++ for Loops.py
@@ -1,13 +1,13 @@
-#performs code on each item in a list i.e. iteration
-#uses a while loop and a counter variable
+# performs code on each item in a list i.e. iteration
+# uses a while loop and a counter variable
 
 words = ["milda", "nigga", "karel", "pavel"]
 counter = 0
-max_index = len(words) - 1
+max_index = len(words) - 1  # python starts counting from 0 so -1 is needed
 
 # print(max_index) now returns 3: range of the list - 1
 
 while counter <= max_index:
     word = words[counter]
-    print(word + "!") #concatenation
+    print(word + "!")  # concatenation
     counter = counter + 1
